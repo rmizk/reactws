9defdd4463eaa200da973ec901e2f0f23f8570e0
Ramzi Hammami <rmizkk@gmail.com>
2021-11-17 01:47:01 +0100

    hassenoob

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Formm from './comp/Formm';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <Formm />
+    
+    </div>
+  );
+}
+
+export default App;
